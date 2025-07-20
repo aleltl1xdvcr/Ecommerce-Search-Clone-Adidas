b38854a4ce843aea45cccc6806335f88773ded50
alexandrordnaxelalexandro@gmail.com <aleltl1xdv>
2025-07-19 18:10:02 -0600

    x

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   images: {
     remotePatterns: [new URL('https://res.cloudinary.com/dg2vnk374/**')],
+    protocol: 'https',
+    hostname: 'res.cloudinary.com',
+    pathname: '/**',
   },
 };
 
